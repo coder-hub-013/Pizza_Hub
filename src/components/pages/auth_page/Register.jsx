64e9e8fd1f593f298d87cb67064cb8e9185b1f7e
coder-hub-013 <158470984+coder-hub-013@users.noreply.github.com>
2026-08-15 17:03:24 +0530

    Update Register.jsx

--- src/components/pages/auth_page/Register.jsx
+++ src/components/pages/auth_page/Register.jsx
@@ -37,13 +37,9 @@ export default function Register() {
 
     };
 
-    const isValidEmail = (email) => {
-
-        const emailRegex =
-            /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+   const isValidEmail = (email) => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ ;
         return emailRegex.test(email);
-
     };
 
 
