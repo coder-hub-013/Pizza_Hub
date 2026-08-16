9adcd844c1cca90ddcfa1c2bf068569cf803b253
coder-hub-013 <158470984+coder-hub-013@users.noreply.github.com>
2026-08-16 14:58:44 +0530

    Update socket.js

--- src/components/utils/socket.js
+++ src/components/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://pizza-hub-backend-10ya.onrender.com", {
     withCredentials: true,
 });
 
